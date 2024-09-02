13d5c8c53b944d98a7acc335b4d1c3ba049f0557
NiallMcguire <mundanelarch@gmail.com>
2024-09-02 12:59:09 +0100

    adding dataset files

--- eval_decoding.py
+++ eval_decoding.py
@@ -178,23 +178,19 @@ if __name__ == '__main__':
     ''' set up dataloader '''
     whole_dataset_dicts = []
     if 'task1' in task_name:
-        dataset_path_task1 = 'datasets/ZuCo/task1-SR/pickle/task1-SR-dataset.pickle'
-        dataset_path_task1=os.path.join(home_directory,dataset_path_task1)
+        dataset_path_task1 = '/users/gxb18167/Datasets/ZuCo/task1-SR/pickle/task1-SR-dataset.pickle'
         with open(dataset_path_task1, 'rb') as handle:
             whole_dataset_dicts.append(pickle.load(handle))
     if 'task2' in task_name:
-        dataset_path_task2 = 'datasets/ZuCo/task2-NR/pickle/task2-NR-dataset.pickle'
-        dataset_path_task2=os.path.join(home_directory,dataset_path_task2)
+        dataset_path_task2 = '/users/gxb18167/Datasets/ZuCo/task2-NR/pickle/task2-NR-dataset.pickle'
         with open(dataset_path_task2, 'rb') as handle:
             whole_dataset_dicts.append(pickle.load(handle))
     if 'task3' in task_name:
-        dataset_path_task3 = 'datasets/ZuCo/task3-TSR/pickle/task3-TSR-dataset.pickle'
-        dataset_path_task3=os.path.join(home_directory,dataset_path_task3)
+        dataset_path_task3 = '/users/gxb18167/Datasets/ZuCo/task3-TSR/pickle/task3-TSR-dataset.pickle'
         with open(dataset_path_task3, 'rb') as handle:
             whole_dataset_dicts.append(pickle.load(handle))
     if 'taskNRv2' in task_name:
-        dataset_path_taskNRv2 = 'datasets/ZuCo/task2-NR-2.0/pickle/task2-NR-2.0-dataset.pickle'
-        dataset_path_taskNRv2=os.path.join(home_directory,dataset_path_taskNRv2)
+        dataset_path_taskNRv2 = '/users/gxb18167/Datasets/ZuCo/task2-NR-2.0/pickle/task2-NR-2.0-dataset.pickle'
         with open(dataset_path_taskNRv2, 'rb') as handle:
             whole_dataset_dicts.append(pickle.load(handle))
     print()
